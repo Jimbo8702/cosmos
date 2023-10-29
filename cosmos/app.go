fa08820493bb96d7fa84487151f3b6fcf978b147
James Sgarella <Jimbo8702@gmail.com>
2023-10-29 17:27:22 -0400

    some clean comments

--- cosmos/app.go
+++ cosmos/app.go
@@ -21,24 +21,39 @@ import (
 const version = "0.1.0"
 
 type Application struct {
-	//config values
+	// Application configuration
 	AppName 		string 
 	Debug 			bool
 	Version 		string
 	RootPath 		string
 	Port 			string
 
-	//internal items
+	// Internal 
+	// 
+	// Application error log
 	ErrorLog 		logger.Logger
+	//
+	// Application info log
 	InfoLog  		logger.Logger
+	//
+	// Application render engine
 	Engine 			render.Renderer
+	//
+	// Application database connection
 	Database 		*database.Database[database.Pool]
-
-	//third party items
+	//
+	// THIRD PARTY DEPS
+	//
+	// Chi mux for routing
 	Router	 		*chi.Mux
+	//
+	// scs session manager for handling secure sessions
 	Session	 		*scs.SessionManager
+	// might make this an interface to allow for different kinds of session management
+	// idk tho thats a lot
 }
 
+// Build a new app with config vars loaded from env
 func NewApp(c *config.Config) *Application {
 	return &Application{
 		RootPath: c.RootPath,
@@ -49,6 +64,7 @@ func NewApp(c *config.Config) *Application {
 	}
 }
 
+// Starts the http server 
 func (a *Application) ListenAndServe() error {
 	w := a.ErrorLog.GetLogWriter()
 	srv := &http.Server{
@@ -59,8 +75,11 @@ func (a *Application) ListenAndServe() error {
 		ReadTimeout: 30 * time.Second,
 		WriteTimeout: 600 * time.Second,
 	}
+	// figure out how to capture errors with the defer
+	// defer closing database connection
 	defer a.CloseDB()
 
+	// log port and return listen and serve from the server 
 	a.InfoLog.Log("Listening on port %s", a.Port)
 	return srv.ListenAndServe()
 }
@@ -68,18 +87,24 @@ func (a *Application) ListenAndServe() error {
 func  (a *Application) CloseDB() error {
 	var err error
 	switch a.Database.Type {
+
+	// Handle closing the sql connection
 	case "postgres", "postgresql":
 		db, ok := a.Database.Pool.(*sql.DB)
 		if !ok {
 			return errors.New("unable to cast to sql client with sql db type")
 		}
 		err = db.Close()
+
+	// Handle closing the mongo connection
 	case "mongo":
 		db, ok := a.Database.Pool.(*mongo.Client)
 		if !ok {
 			return errors.New("unable to cast to mongo client with mongo db type")
 		}
 		err = db.Disconnect(context.Background())
+
+	// type unsupported, technically should never get here 
 	default:
 		err = errors.New("couldn't find db type while closing")
 	}
